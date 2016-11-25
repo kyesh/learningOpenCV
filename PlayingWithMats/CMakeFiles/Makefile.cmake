385f8ea16b16c3310c598c8cba3fed7a7a95795d
ken <kenyesh2@gmail.com>
2016-11-25 11:39:57 -0500

    I probably added way too many grabage files

--- PlayingWithMats/CMakeFiles/Makefile.cmake
+++ PlayingWithMats/CMakeFiles/Makefile.cmake
@@ -43,5 +43,5 @@ SET(CMAKE_MAKEFILE_PRODUCTS
 
 # Dependency information for all targets:
 SET(CMAKE_DEPEND_INFO_FILES
-  "CMakeFiles/DisplayImage.dir/DependInfo.cmake"
+  "CMakeFiles/MatDemo.dir/DependInfo.cmake"
   )
